7595c4a084367b94204cbba2d5de3aefab56327c
yazan ghunaim <yazanghunaim07@gmail.com>
2024-09-22 18:53:43 +0200

    Updated .gitignore

---
+++ client/Voyago/Voyago/VoyagoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoyagoApp.swift
+//  Voyago
+//
+//  Created by Yazan Ghunaim on 9/22/24.
+//
+
+import SwiftUI
+
+@main
+struct VoyagoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
