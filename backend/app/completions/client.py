46b778c63062660adcf6f3e473022f84b0fd2446
yazan ghunaim <yazanghunaim07@gmail.com>
2024-12-02 15:32:57 +0100

    cleaning prompt storing, refactoring SighRecommendation models to itineraries

--- backend/app/completions/client.py
+++ backend/app/completions/client.py
@@ -5,7 +5,8 @@ import openai
 from dotenv import load_dotenv
 from openai import LengthFinishReasonError
 
-from app.models.recommendations import RecommendationQuery, SightRecommendation
+from app.models.recommendations import Itinerary, RecommendationQuery
+from app.prompts.prompts import GET_SIGHT_RECOMMENDATIONS_AND_PLAN
 
 load_dotenv()
 
@@ -19,7 +20,7 @@ class AIClient:
         """see class doc"""
         self._client = openai.OpenAI(api_key=os.getenv("OPENAI_KEY"))
 
-    def send_recommendation_query(self, query: RecommendationQuery) -> SightRecommendation | str:
+    def send_recommendation_query(self, query: RecommendationQuery) -> Itinerary | str:
         """
         Takes in a RecommendationsQuery model using openAi's completion parsing
         returns a SightRecommendation model
@@ -33,25 +34,18 @@ class AIClient:
                 messages=[
                     {
                         "role": "system",
-                        "content": """
-                                You are a travel guide, and will provide sight recommendations depending where the user 
-                                wants to go, you will only respond in bullet points with the sight names and nothing else.
-                                
-                                User will only tell you where he wants to go and how many sights he wants from you
-                                ex: (destination: Japan count: 5), then you will say nothing else of what your tasked,
-                                 and just respond with the number of requested sights.
-                                """},
+                        "content": GET_SIGHT_RECOMMENDATIONS_AND_PLAN},
                     {
                         "role": "user",
-                        "content": f"destination: {query.destination} count: {query.count}"
+                        "content": f"destination: {query.destination} days: {query.days}"
                     }
                 ],
-                response_format=SightRecommendation,
-                max_tokens=50
+                response_format=Itinerary,
+                # max_tokens=50
             )
             sight_recommendations = completion.choices[0].message
             if sight_recommendations.refusal:
-                # Handle refusal
+                # TODO: Handle refusal
                 return sight_recommendations.refusal
             return sight_recommendations.parsed
         except LengthFinishReasonError as e:
--- backend/app/models/recommendations.py
+++ backend/app/models/recommendations.py
@@ -3,11 +3,18 @@ from pydantic import BaseModel
 
 
 class SightRecommendation(BaseModel):
+    """model of a single sight recommendation"""
+    sight: str
+    brief: str
+
+
+class Itinerary(BaseModel):
     """model of what is expected from the LLM to return"""
-    recommendations: list[str]
+    recommendations: list[SightRecommendation]
+    plan: str
 
 
 class RecommendationQuery(BaseModel):
     """model of the query the user should send the LLM"""
     destination: str
-    count: int  # number of sights requested
+    days: int  # number of sights requested
