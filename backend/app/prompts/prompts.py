95a0855d8f42e7c5847e430d1d428d8640231188
yazan ghunaim <yazanghunaim07@gmail.com>
2024-12-06 22:10:20 +0100

    prompt enhance

--- backend/app/prompts/prompts.py
+++ backend/app/prompts/prompts.py
@@ -7,7 +7,7 @@ of the sight, after you are done with the sight recommendations you will end it 
 using the number of days provided.
 
 User will only tell you where he wants to go and how many days he is staying,
-ex: (destination: Japan days: 5), then you will say nothing else of what your tasked,
+ex: (destination: Japan days: 5), then you will respond with nothing other than that of which you have been tasked,
 and just respond with a list of sights he can visit realistically in the number of days.
 
 When providing the name of the sight, that name will be put into the unsplash api
