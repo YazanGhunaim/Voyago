cf2c9d9d664a272acb8fd20c1336b41491e4b9f5
yazan ghunaim <yazanghunaim07@gmail.com>
2024-12-07 09:48:00 +0100

    retrieving an image collection functionality

--- backend/app/services/unsplash/unsplash_service.py
+++ backend/app/services/unsplash/unsplash_service.py
@@ -9,6 +9,8 @@ from app.config.config import get_config
 log = getLogger(__name__)
 
 
+# TODO: Super important! need to retrieve metadata of images as well like user profile, location etc for unsplash legal reasons
+
 class UnsplashService:
     """Unsplash service class
 
@@ -19,6 +21,7 @@ class UnsplashService:
         """see class doc"""
         self.config = get_config()
         self.client_id = self.config.unsplash_key  # secret access key
+        self.base_url = self.config.unsplash_base_url
 
     def fetch_image_for(self, sight: str, count: int = 5) -> list[str]:
         """fetches image for a given sight
@@ -34,7 +37,7 @@ class UnsplashService:
                 "client_id": self.client_id
             }
 
-            response = requests.get("https://api.unsplash.com/search/photos", params=params)
+            response = requests.get(f"{self.base_url}/search/photos", params=params)
             response.raise_for_status()
 
             data = response.json()
@@ -45,4 +48,33 @@ class UnsplashService:
             return []
         except Exception as e:
             log.error(f"Unknown error encountered while fetching images for {sight}, failed with exception: {e}")
+            return []
+
+    def fetch_image_collection_for(self, topic: str, count: int = 10, page: int = 1) -> list[str]:
+        """fetches an image collection for a curated topic
+
+        :param topic: topic requested for image collection
+        :param count: number of images requested
+        :param page: page number [pagination purposes]
+        :return: list of image url's
+        """
+        try:
+            params = {
+                "query": topic,
+                "per_page": count,
+                "page": page,
+                "client_id": self.client_id
+            }
+
+            response = requests.get(f"{self.base_url}/search/collections", params=params)
+            response.raise_for_status()
+
+            data = response.json()
+            images = [photo["cover_photo"]["urls"]["regular"] for photo in data["results"]]  # get images
+            return images if response.ok else []
+        except HTTPException as e:
+            log.error(f"Error fetching image collection for {topic}, failed with exception: {e}")
+            return []
+        except Exception as e:
+            log.error(f"Unknown error encountered while fetching image collection for {topic}, with exception: {e}")
             return []
