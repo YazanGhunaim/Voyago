15eb62184b90ab4af02ab38f1b156e4dacc3ce8d
yazan ghunaim <yazanghunaim07@gmail.com>
2025-01-04 22:34:41 +0100

    ImageCard component
    pagination implementation in homeview

--- voyago_iOS/voyago_iOS/Core/Home/Views/HomeView.swift
+++ voyago_iOS/voyago_iOS/Core/Home/Views/HomeView.swift
@@ -9,32 +9,64 @@ import Kingfisher
 import SwiftUI
 
 struct HomeView: View {
-    @State private var viewModel = HomeViewModel()
+    @State private var viewModel = HomeViewModel(keywords: [
+        "Travel", "Cities", "Nature", "Tourism",
+    ])
     let columns = [GridItem(.flexible()), GridItem(.flexible())]
 
     var body: some View {
-        if self.viewModel.succeeded {
-            NavigationStack {
-                ScrollView {
-                    LazyVGrid(columns: columns, spacing: 20) {
-                        ForEach(self.viewModel.images, id: \.self) { image in
-                            KFImage(URL(string: image))
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 150, height: 200)
-                                .clipShape(.rect(cornerRadius: 5))
-                        }
-                    }
-                    .padding(.horizontal)
-                }
-                .navigationTitle("Voyago")
+        NavigationStack {
+            switch self.viewModel.viewState {
+            case .Loading:
+                ProgressView()
+            case .Failure:
+                // TODO: Error view
+                Text("Error Encountered.")
+            case _:
+                HomeScrollView(viewModel: self.viewModel, columns: self.columns)
             }
-        } else {
-            ProgressView()
         }
     }
 }
 
 #Preview {
     HomeView()
+}
+
+struct HomeScrollView: View {
+    let viewModel: HomeViewModel
+    let columns: [GridItem]
+
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: columns) {
+                ForEach(self.viewModel.imageUrls, id: \.self) {
+                    imageUrl in
+
+                    // TODO: PASS METADATA
+                    ImageCard(imageUrl: imageUrl)
+                        .onAppear {
+                            if self.viewModel.lastImage(
+                                imageUrl: imageUrl)
+                                && self.viewModel.viewState != .Fetching
+                            {
+                                Task {
+                                    await self.viewModel.getMoreImages()
+                                }
+                            }
+                        }
+                }
+            }
+            .overlay(
+                alignment: .bottom,
+                content: {
+                    if self.viewModel.viewState == .Fetching {
+                        ProgressView()
+                    }
+                }
+            )
+            .padding(.horizontal)
+        }
+        .navigationTitle("Voyago")
+    }
 }
